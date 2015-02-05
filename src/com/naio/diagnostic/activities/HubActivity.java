bbe0583af01b51172f1068328b293cb9fe367b9f
vbodlloyd <vbodlloyd@gmail.com>
2015-02-05 17:53:57 +0100

    creation of the hub activity

---
+++ src/com/naio/diagnostic/activities/HubActivity.java
@@ -0,0 +1,41 @@
+package com.naio.diagnostic.activities;
+
+import com.naio.diagnostic.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+
+public class HubActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getActionBar().setBackgroundDrawable(
+				getResources().getDrawable(R.drawable.form));
+		setContentView(R.layout.hub_activity);
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		// TODO Auto-generated method stub
+		super.onCreateContextMenu(menu, v, menuInfo);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void go_to_lidar_gps_control_activity(View v){
+		Intent intent = new Intent(this, LidarGPSMotorsActivity.class);
+
+		startActivity(intent);
+	}
+}
--- src/com/naio/diagnostic/utils/Config.java
+++ src/com/naio/diagnostic/utils/Config.java
@@ -8,7 +8,10 @@ public class Config {
 	public static final int PORT_LIDAR = 4337;
 	public static final int PORT_MOTORS = 4331;
 	public static final int PORT_REMOTE = 4338;
-	public static final int PORT_TEST = 4444;
+	public static final int PORT_ACCELERO = 5334;
+	public static final int PORT_MAGNETO = 5337;
+	public static final int PORT_GYRO = 5331;
+	public static final int PORT_ODO = 5338;
 
 	public static final int LENGHT_HEADER = 6;
 	public static final int LENGHT_ID = 1;
@@ -17,12 +20,23 @@ public class Config {
 
 	public static final int LENGHT_TRAME_GPS = 43;
 	public static final int LENGHT_TRAME_LIDAR = 813;
-	public static final int LENGHT_TRAME_REMOTE = 14;
 	public static final int LENGHT_TRAME_MOTORS = 2;
+	public static final int LENGHT_TRAME_ACCELERO = 6;
+	public static final int LENGHT_TRAME_ACTUATOR = 1;
+	public static final int LENGHT_TRAME_GYRO = 6;
+	public static final int LENGHT_TRAME_MAGNETO = 6;
+	public static final int LENGHT_TRAME_ODO = 4;
+	public static final int LENGHT_TRAME_REMOTE = 14;
 
-	public static final int ID_GPS = 4;
-	public static final int ID_LIDAR = 7;
 	public static final int ID_MOTORS = 1;
+	public static final int ID_GPS = 4;
+	public static final int ID_ODO = 5;
+	public static final int ID_LIDAR = 7;
+	public static final int ID_REMOTE = 8;
+	public static final int ID_ACCELERO = 9;
+	public static final int ID_GYRO = 10;
+	public static final int ID_MAGNETO = 11;
+	public static final int ID_ACTUATOR = 12;
 
 	public static final int BUFFER_SIZE = 2048;
 }
