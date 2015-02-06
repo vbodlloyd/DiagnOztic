05a53a9125bd00d8dd0ac9d4cd811d7f4815f231
vbodlloyd <vbodlloyd@gmail.com>
2015-02-06 13:44:55 +0100

    add layout transition

--- src/com/naio/diagnostic/activities/HubActivity.java
+++ src/com/naio/diagnostic/activities/HubActivity.java
@@ -35,7 +35,7 @@ public class HubActivity extends Activity {
 	
 	public void go_to_lidar_gps_control_activity(View v){
 		Intent intent = new Intent(this, LidarGPSMotorsActivity.class);
-
 		startActivity(intent);
+		overridePendingTransition(R.animator.animation1, R.animator.animation2);
 	}
 }
